a228a89199ae6b865c8b03c230eca44bf4060e9b
Kevin Dai <kevin.dai.2451@gmail.com>
2020-09-19 14:42:34 -0400

    added java solution

---
+++ insertintoabinarysearchtree.java
@@ -0,0 +1,19 @@
+//Java Solution
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        
+        if (root.val >= val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        
+        return root;
+    }
+}
